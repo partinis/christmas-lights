471625fefa0180f21ce2df2c98d036bcbfae3959
jasineri <jasineri@gmx.de>
2024-12-26 17:24:36 +0100

    init

--- Internet-Xmas-Tree/lights/lights.py
+++ Internet-Xmas-Tree/lights/lights.py
@@ -71,6 +71,7 @@ if __name__ == '__main__':
     # initialize the led strip
     with neopixel.NeoPixel(board.D18, int(conf.count), bpp=3, auto_write=False, brightness=conf.brightness, pixel_order=neopixel.GRB) as strip:
         state = State()
+        state.pattern = 2
         try:
             while True:
                 print("Calling function "+str(state.pattern))
