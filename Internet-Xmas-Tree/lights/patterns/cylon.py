d9534a3dc01a20768c5834ce712f0118ce47a49f
jasineri <jasineri@gmx.de>
2024-12-27 00:09:34 +0100

    init

--- Internet-Xmas-Tree/lights/patterns/cylon.py
+++ Internet-Xmas-Tree/lights/patterns/cylon.py
@@ -1,15 +1,9 @@
-"""
-"Cylon" pattern
-"""
-
 from .pattern import Pattern
 import time
 import math
+from .putil import *
 
 class Cylon(Pattern):
-
-    def __init__(self):
-        pass
 
     @classmethod
     def get_id(self):
@@ -24,22 +18,21 @@ class Cylon(Pattern):
         blue = 0
         SpeedDelay = .1
         ReturnDelay = .5
-        # set all to color2
         for i in range (0, (LED_COUNT - EyeSize - 2)):
-            strip.fill((0, 0, 0))
-            strip[i] = (int(math.floor(red / 10)), int(math.floor(green / 10)), int(math.floor(blue / 10)))
+            SetAll(strip, Color(0, 0, 0))
+            strip.setPixelColor(i, Color(int(math.floor(red / 10)), int(math.floor(green / 10)), int(math.floor(blue / 10))))
             for j in range(1, (EyeSize + 1)):
-                strip[i + j] = (red, green, blue)
-            strip[i + EyeSize + 1] = (int(math.floor(red / 10)), int(math.floor(green / 10)), int(math.floor(blue / 10)))
+                strip.setPixelColor(i + j, Color(red, green, blue))
+            strip.setPixelColor(i + EyeSize + 1, Color(int(math.floor(red / 10)), int(math.floor(green / 10)), int(math.floor(blue / 10))))
             strip.show()
             time.sleep(SpeedDelay)
         time.sleep(ReturnDelay)
         for i in range ((LED_COUNT - EyeSize - 2), 0, -1):
-            strip.fill((0, 0, 0))
-            strip[i] = (int(math.floor(red / 10)), int(math.floor(green / 10)), int(math.floor(blue / 10)))
+            SetAll(strip, Color(0, 0, 0))
+            strip.setPixelColor(i, Color(int(math.floor(red / 10)), int(math.floor(green / 10)), int(math.floor(blue / 10))))
             for j in range(1, (EyeSize + 1)):
-                strip[i + j] = (red, green, blue)
-            strip[i + EyeSize + 1] = (int(math.floor(red / 10)), int(math.floor(green / 10)), int(math.floor(blue / 10)))
+                strip.setPixelColor(i + j, Color(red, green, blue))
+            strip.setPixelColor(i + EyeSize + 1, Color(int(math.floor(red / 10)), int(math.floor(green / 10)), int(math.floor(blue / 10))))
             strip.show()
             time.sleep(SpeedDelay)
         time.sleep(ReturnDelay)
