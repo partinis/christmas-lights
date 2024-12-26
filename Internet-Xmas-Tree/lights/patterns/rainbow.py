425a88553068d268415d63203de0ac12a31ba109
jasineri <jasineri@gmx.de>
2024-12-26 19:22:29 +0100

    init

--- Internet-Xmas-Tree/lights/patterns/rainbow.py
+++ Internet-Xmas-Tree/lights/patterns/rainbow.py
@@ -5,6 +5,7 @@
 from .pattern import Pattern
 import time
 import math
+import Color
 
 class Cylon(Pattern):
 
@@ -19,11 +20,22 @@ class Cylon(Pattern):
     def update(self, strip, state):
         LED_COUNT = len(strip)
         SpeedDelay = 0
-        for i in range(0, 256):
-            for j in range(0, LED_COUNT):
-                strip[j] = Wheel((j + i) & 255)
+        for j in range(256):
+            for i in range(strip.numPixels()):
+                strip.setPixelColor(i, wheel((i + j) & 255))
             strip.show()
-            time.sleep(SpeedDelay)
+            time.sleep(20 / 1000.0)
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
 
 def Wheel(WheelPosition):
     #Generate rainbow colors across 0-255 positions.
