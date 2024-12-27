a3d54eccf899389f965c68636e767279db4ab2bb
jasineri <jasineri@gmx.de>
2024-12-27 13:48:29 +0100

    init

--- Internet-Xmas-Tree/lights/patterns/random.py
+++ Internet-Xmas-Tree/lights/patterns/random.py
@@ -15,8 +15,8 @@ class Random(Pattern):
     @classmethod
     def update(self, strip, state):
         for _ in range(50):
-            index = random.randint(0, len(strip) - 1)
-            strip[index] = get_random_color()
+            for i in range(10):
+                strip[get_random_pixel()] = get_random_color()
             strip.show()
             time.sleep(0.1)
-            strip[index] = (0, 0, 0)
+            strip.fill((0, 0, 0))
