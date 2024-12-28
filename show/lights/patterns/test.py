45c8e0a45e1629abb82a681d567544f99436e949
jasineri <jasineri@gmx.de>
2024-12-28 19:10:12 +0100

    init

--- show/lights/patterns/test.py
+++ show/lights/patterns/test.py
@@ -13,15 +13,12 @@ class Test(Pattern):
 
     @classmethod
     def update(self, strip, state):
-        color = (255, 255, 255)
-        for i in range(NUM_PIXELS):
-            strip.setPixelColor(i, (0, 0, 0))  # Clear all LEDs
-        snowflakes = [random.randint(0, NUM_PIXELS - 1) for _ in range(5)]  # Random starting points
-        for _ in range(NUM_PIXELS):
-            for i in range(len(snowflakes)):
-                if snowflakes[i] > 0:
-                    strip.setPixelColor(snowflakes[i], (0, 0, 0))  # Turn off old position
-                    snowflakes[i] -= 1
-                    strip.setPixelColor(snowflakes[i], color)  # Set new position
+        for _ in range(10):
+            for i in range(NUM_PIXELS):
+                if random.random() < 0.1:  # 10% chance to sparkle
+                    color = random.choice([(0, 255, 0), (255, 0, 0), (255, 255, 0)])  # Green, Red, Yellow
+                    strip.setPixelColor(i, color)
+                else:
+                    strip.setPixelColor(i, (0, 0, 0))
             strip.show()
             time.sleep(0.1)
