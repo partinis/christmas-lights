5da3d70c92629e2d5092a92260acc27766eab3be
jasineri <jasineri@gmx.de>
2024-12-28 16:44:50 +0100

    init

--- show/lights/patterns/scroll.py
+++ show/lights/patterns/scroll.py
@@ -17,15 +17,16 @@ class Scroll(Pattern):
 
     @classmethod
     def update(self, strip, state):
-        # iterate through
-        offset = int(time.time() * 1000 / float(state.delay))
-        for x in range(len(strip)):
-            # default color 1
-            color = state.color1
-            # offset by time, x pos divided by length mod 2
-            # time.time is in seconds not ms
-            # sets color 2
-            if ((offset + x) // state.length) % 2 == 0:
-                color = state.color2
-            # set color
-            strip[x] = color
+        for _ in range(100):
+            # iterate through
+            offset = int(time.time() * 1000 / float(state.delay))
+            for x in range(len(strip)):
+                # default color 1
+                color = state.color1
+                # offset by time, x pos divided by length mod 2
+                # time.time is in seconds not ms
+                # sets color 2
+                if ((offset + x) // state.length) % 2 == 0:
+                    color = state.color2
+                # set color
+                strip[x] = color
