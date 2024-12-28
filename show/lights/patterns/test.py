1408715641eec7db9f03d957864a201e80c09fe7
jasineri <jasineri@gmx.de>
2024-12-28 19:17:15 +0100

    init

--- show/lights/patterns/test.py
+++ show/lights/patterns/test.py
@@ -13,12 +13,11 @@ class Test(Pattern):
 
     @classmethod
     def update(self, strip, state):
-        color = (0,0,255)
-        for _ in range(5):  # Number of fireworks
+        for _ in range(20):  # Number of fireworks
             burst_center = random.randint(0, NUM_PIXELS - 1)
             for radius in range(1, 10):  # Expand outward
                 for i in range(max(0, burst_center - radius), min(NUM_PIXELS, burst_center + radius)):
-                    strip.setPixelColor(i, color)
+                    strip.setPixelColor(i, get_random_color())
                 strip.show()
                 time.sleep(.05)
                 for i in range(max(0, burst_center - radius), min(NUM_PIXELS, burst_center + radius)):
