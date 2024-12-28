b32822ee9c70d5e75d7a5f48f87b5733c2a18b99
jasineri <jasineri@gmx.de>
2024-12-28 19:38:46 +0100

    init

--- show/lights/patterns/test.py
+++ show/lights/patterns/test.py
@@ -22,9 +22,11 @@ class Test(Pattern):
         gravity=0.8
         center = random.randint(0, NUM_PIXELS - 1)
         center = random.randint(0, NUM_PIXELS - 1)
-        for _ in range(100):
-            led = random.randint(0, NUM_PIXELS - 1)
-            color = (0, 255, 0) if random.random() < 0.5 else (255, 0, 0)
-            strip.setPixelColor(led, color)
+        for j in range(256):
+            for i in range(NUM_PIXELS):
+                if (i + j) % 2 == 0:
+                    strip.setPixelColor(i, color1)
+                else:
+                    strip.setPixelColor(i, color2)
             strip.show()
             time.sleep(delay)
