0d332f550827782fbbd7dbd0db753a69e25d2d91
jasineri <jasineri@gmx.de>
2024-12-28 19:39:14 +0100

    init

--- show/lights/patterns/test.py
+++ show/lights/patterns/test.py
@@ -22,11 +22,16 @@ class Test(Pattern):
         gravity=0.8
         center = random.randint(0, NUM_PIXELS - 1)
         center = random.randint(0, NUM_PIXELS - 1)
-        for j in range(256):
+        snow = [0] * NUM_PIXELS
+        for _ in range(100):
+            snow[random.randint(0, NUM_PIXELS - 1)] = 1
+            for i in range(NUM_PIXELS - 1, 0, -1):
+                snow[i] = snow[i - 1]
+            snow[0] = 0
             for i in range(NUM_PIXELS):
-                if (i + j) % 2 == 0:
-                    strip.setPixelColor(i, color1)
+                if snow[i]:
+                    strip.setPixelColor(i, (255, 255, 255))
                 else:
-                    strip.setPixelColor(i, color2)
+                    strip.setPixelColor(i, (0, 0, 0))
             strip.show()
             time.sleep(delay)
