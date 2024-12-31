f95b1e9180757a07ab63d4dd919ab759816e92e3
jasineri <jasineri@gmx.de>
2024-12-31 17:36:52 +0100

    init

--- show/lights/patterns/test.py
+++ show/lights/patterns/test.py
@@ -35,7 +35,7 @@ class Test(Pattern):
 
             # Set the "eye" of the effect
             for j in range(1, EyeSize + 1):
-                strip.setPixelColor(i + j, Color(red, green, blue))
+                strip.setPixelColor(i + j, (red, green, blue))
 
             # Set the trailing dim pixel
             #strip.setPixelColor(i + EyeSize + 1, Color(red // 10, green // 10, blue // 10))
